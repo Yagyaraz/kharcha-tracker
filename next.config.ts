a9f1306dcea1e3b125616857d98e4c1203fb176c
Yagya Raz <yagyaraz28@gmail.com>
2026-05-25 14:50:28 +0545

    CORS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow testing from devices on the local network
+  // @ts-ignore - The property might not be perfectly typed in this version
+  allowedDevOrigins: ["192.168.88.172", "localhost"],
 };
 
 export default nextConfig;
